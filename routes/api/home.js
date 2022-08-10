a90c39e327f06e60ee77d3c20f2f716ff17d8549
walrus22 <104523755+walrus22@users.noreply.github.com>
2022-08-10 20:57:52 +0900

    1234

--- routes/api/home.js
+++ routes/api/home.js
@@ -7,18 +7,21 @@ const ObjectId = require("mongodb").ObjectId;
 
 let tzoffset = new Date().getTimezoneOffset() * 60000; //offset in milliseconds
 let today = (new Date(Date.now() - tzoffset)).toISOString().slice(0, -1);
-let yesterday = (new Date(Date.now() - tzoffset - 60000*60*24*5)).toISOString().slice(0, -1); // *%5지워
+let yesterday = (new Date(Date.now() - tzoffset - 60000*60*24*10)).toISOString().slice(0, -1); // *%5지워
 
 
 router.get('/home', (req,res)=>{
   // console.log(today)
   // console.log(yesterday)
-  Platform.find({$and : [
-    {update_time : {
-      "$lte" : today, 
-      "$gte" : yesterday,
-    }},
-    {rank: 1}]})
+  // Platform.find({$and : [
+  //   {update_time : {
+  //     "$lte" : today, 
+  //     "$gte" : yesterday,
+  //   }},
+  //   {rank: 1}]})
+
+  
+  Platform.find({rank: 1})
   .populate('webtoon._id')
   .then(webtoons => res.json(webtoons))
   .catch(err => res.status(404).json({ nobooksfound: 'No Webtoons found' }));
