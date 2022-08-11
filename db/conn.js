96111408bbab88d385903a9effdb80ae1e8636b0
walrus22 <104523755+walrus22@users.noreply.github.com>
2022-08-11 20:51:15 +0900

    1234

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const Db = process.env.ATLAS_URI;
+const Db = process.env.MONGO_URI;
 
 // const { MongoClient } = require("mongodb");
 // const client = new MongoClient(Db, {
