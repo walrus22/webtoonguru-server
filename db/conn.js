49b0bab651a2da18ee5ffc2b42cd3de0b83b446e
walrus22 <kss24111@gmail.com>
2022-08-15 00:14:14 +0900

    123

--- db/conn.js
+++ db/conn.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const Db = process.env.MONGO_DB_URL;
+const Db = process.env.MONGO_URI;
 
 // const { MongoClient } = require("mongodb");
 // const client = new MongoClient(Db, {
